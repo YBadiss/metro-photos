c3639a250cdd62677a5e1bc6802f0b1ad780aa04
Yacine Badiss <yacine.badiss@gmail.com>
2025-12-30 13:04:58 +0100

    feat: finalise config

--- app/front/deploy.sh
+++ app/front/deploy.sh
@@ -10,12 +10,8 @@ timestamp=$(date +%s)
 # Create target directory with proper permissions
 ssh $SSH_OPTIONS root@167.71.143.97 "mkdir -p /var/www/metro-boulot.photos-${timestamp} && chmod 755 /var/www/metro-boulot.photos-${timestamp}"
 
-# Deploy the project
+# Deploy the built static files
 scp $SSH_OPTIONS -r dist/* root@167.71.143.97:/var/www/metro-boulot.photos-${timestamp}/
-scp $SSH_OPTIONS package*.json root@167.71.143.97:/var/www/metro-boulot.photos-${timestamp}/
 
-# Install dependencies
-ssh $SSH_OPTIONS root@167.71.143.97 "cd /var/www/metro-boulot.photos-${timestamp} && npm i --omit=dev"
-
-# Create a new symlink
-ssh $SSH_OPTIONS root@167.71.143.97 "(rm /var/www/metro-boulot.photos || true) && ln -s /var/www/metro-boulot.photos-${timestamp} /var/www/metro-boulot.photos"
+# Create a new symlink (remove old one if exists)
+ssh $SSH_OPTIONS root@167.71.143.97 "rm -f /var/www/metro-boulot.photos && ln -s /var/www/metro-boulot.photos-${timestamp} /var/www/metro-boulot.photos"
