d4a3985548196b6eca9bb149a03f4e40dfca4fbc
Yacine Badiss <yacine.badiss@gmail.com>
2025-12-30 13:17:13 +0100

    feat: remove empty stations

--- data/main.py
+++ data/main.py
@@ -7,7 +7,7 @@ import requests
 import xml.etree.ElementTree as ET
 from collections import Counter
 import re
-    
+
 
 SOURCE = Path("./source")
 GENERATED = Path("./generated/")
@@ -22,7 +22,12 @@ def extract_dominant_color_from_svg(svg_path: Path) -> str:
     for elem in root.iter():
         # Check fill attribute
         fill = elem.get("fill")
-        if fill and fill != "none" and fill.startswith('#') and fill.lower() != "#ffffff":
+        if (
+            fill
+            and fill != "none"
+            and fill.startswith("#")
+            and fill.lower() != "#ffffff"
+        ):
             colors.append(fill.lower())
         # Check style attribute
         style = elem.get("style")
@@ -81,16 +86,23 @@ def extract_lines():
         ta = TypeAdapter(list[ZoneLineRelationship])
         line_relationships = ta.validate_json(f.read())
     lines_set = {
-        Line(id=relationship.line_id, name=relationship.line_name, icon_url=relationship.line_icon_url, icon_filename=relationship.line_icon_filename)
+        Line(
+            id=relationship.line_id,
+            name=relationship.line_name,
+            icon_url=relationship.line_icon_url,
+            icon_filename=relationship.line_icon_filename,
+        )
         for relationship in line_relationships
     }
     lines = sorted(list(lines_set), key=lambda line: line.name)
     for line in lines:
         if line.icon_url and line.icon_filename:
             img_data = requests.get(line.icon_url).content
-            with open(GENERATED_ICONS / line.icon_filename, 'wb') as f:
+            with open(GENERATED_ICONS / line.icon_filename, "wb") as f:
                 f.write(img_data)
-            line.color = extract_dominant_color_from_svg(GENERATED_ICONS / line.icon_filename)
+            line.color = extract_dominant_color_from_svg(
+                GENERATED_ICONS / line.icon_filename
+            )
 
     with open(GENERATED / "lines.json", "w") as f:
         ta = TypeAdapter(list[Line])
@@ -113,7 +125,7 @@ def combine_data():
     with open(GENERATED / "zones_lines_rel.json", "r") as f:
         ta = TypeAdapter(list[ZoneLineRelationship])
         lines_relationships = ta.validate_json(f.read())
-    
+
     accesses_by_zone = defaultdict(list)
     for relationship in accesses_relationships:
         access = next(a for a in accesses if a.id == relationship.access_id)
@@ -123,7 +135,7 @@ def combine_data():
     for relationship in lines_relationships:
         if relationship.mode != "METRO":
             continue
-        line = next(l for l in lines if l.id == relationship.line_id)
+        line = next(line for line in lines if line.id == relationship.line_id)
         lines_by_zone[relationship.zone_id].append(line)
 
     zones_metro = [z for z in zones if z.type == "metroStation"]
@@ -133,7 +145,11 @@ def combine_data():
 
     with open(GENERATED / "zones_metro.json", "w") as f:
         ta = TypeAdapter(list[Zone])
-        f.write(ta.dump_json(zones_metro, indent=4).decode())
+        f.write(
+            ta.dump_json(
+                [zm for zm in zones_metro if zm.accesses and zm.lines], indent=4
+            ).decode()
+        )
 
 
 if __name__ == "__main__":
--- data/models.py
+++ data/models.py
@@ -1,4 +1,11 @@
-from pydantic import BaseModel, Field, AliasChoices, AliasPath, ConfigDict, field_validator
+from pydantic import (
+    BaseModel,
+    Field,
+    AliasChoices,
+    AliasPath,
+    ConfigDict,
+    field_validator,
+)
 
 
 class GeoPoint(BaseModel):
@@ -11,10 +18,18 @@ class Access(BaseModel):
 
     id: str = Field(validation_alias=AliasChoices("accid", "id"))
     name: str = Field(validation_alias=AliasChoices("accname", "name"))
-    short_name: int | None = Field(validation_alias=AliasChoices("accshortname", "short_name"))
-    x_lambert_93: int = Field(validation_alias=AliasChoices("accxespg2154", "x_lambert_93"))
-    y_lambert_93: int = Field(validation_alias=AliasChoices("accyespg2154", "y_lambert_93"))
-    geo_point: GeoPoint = Field(validation_alias=AliasChoices("accgeopoint", "geo_point"))
+    short_name: int | None = Field(
+        validation_alias=AliasChoices("accshortname", "short_name")
+    )
+    x_lambert_93: int = Field(
+        validation_alias=AliasChoices("accxespg2154", "x_lambert_93")
+    )
+    y_lambert_93: int = Field(
+        validation_alias=AliasChoices("accyespg2154", "y_lambert_93")
+    )
+    geo_point: GeoPoint = Field(
+        validation_alias=AliasChoices("accgeopoint", "geo_point")
+    )
 
 
 class Line(BaseModel):
@@ -35,9 +50,15 @@ class Zone(BaseModel):
     name: str = Field(validation_alias=AliasChoices("zdaname", "name"))
     type: str | None = Field(validation_alias=AliasChoices("zdatype", "type"))
     town: str = Field(validation_alias=AliasChoices("zdatown", "town"))
-    postal_region: str = Field(validation_alias=AliasChoices("zdapostalregion", "postal_region"))
-    x_lambert_93: int = Field(validation_alias=AliasChoices("zdaxepsg2154", "x_lambert_93"))
-    y_lambert_93: int = Field(validation_alias=AliasChoices("zdayepsg2154", "y_lambert_93"))
+    postal_region: str = Field(
+        validation_alias=AliasChoices("zdapostalregion", "postal_region")
+    )
+    x_lambert_93: int = Field(
+        validation_alias=AliasChoices("zdaxepsg2154", "x_lambert_93")
+    )
+    y_lambert_93: int = Field(
+        validation_alias=AliasChoices("zdayepsg2154", "y_lambert_93")
+    )
     accesses: list[Access] = []
     lines: list[Line] = []
 
@@ -61,7 +82,9 @@ class ZoneLineRelationship(BaseModel):
     )
     line_icon_filename: str | None = Field(
         default=None,
-        validation_alias=AliasChoices(AliasPath("picto", "filename"), "line_icon_filename"),
+        validation_alias=AliasChoices(
+            AliasPath("picto", "filename"), "line_icon_filename"
+        ),
     )
     mode: str
 
